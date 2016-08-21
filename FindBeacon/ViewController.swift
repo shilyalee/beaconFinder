cdffcfd5e2bcfb0cb995987f246f0834a231f068
YUN TZU LEE <welkinlee@gmail.com>
2016-08-21 16:30:45 -0700

    Initial Commit

---
+++ FindBeacon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FindBeacon
+//
+//  Created by Yun Tzu Lee on 8/21/16.
+//  Copyright © 2016 Yun Tzu Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
